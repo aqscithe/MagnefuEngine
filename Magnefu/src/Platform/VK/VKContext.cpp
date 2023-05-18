c69eb323e562609c52864ff363cb4bfad23c88c7
Leon Thompson <ltyronethompson09@gmail.com>
2023-05-18 11:40:38 -0600

    implementing vulkan swap chain

--- Magnefu/src/Platform/VK/VKContext.cpp
+++ Magnefu/src/Platform/VK/VKContext.cpp
@@ -5,6 +5,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <set>
+
 
 namespace Magnefu
 {
@@ -18,6 +20,8 @@ namespace Magnefu
 
 	VKContext::~VKContext()
 	{
+		vkDestroySwapchainKHR(m_VkDevice, m_SwapChain, nullptr);
+		vkDestroySurfaceKHR(m_VkInstance, m_WindowSurface, nullptr);
 		vkDestroyInstance(m_VkInstance, nullptr);
 		vkDestroyDevice(m_VkDevice, nullptr);
 	}
@@ -130,7 +134,8 @@ namespace Magnefu
 			MF_CORE_DEBUG("Vulkan not Supported!!");
 		}
 		
-		glfwCreateWindowSurface(m_VkInstance, m_WindowHandle, nullptr, &m_WindowSurface);
+		if (glfwCreateWindowSurface(m_VkInstance, m_WindowHandle, nullptr, &m_WindowSurface) != VK_SUCCESS)
+			MF_CORE_ASSERT(false, "Failed to create a window surface!");
 
 		// -------------------------------- //
 		
@@ -164,16 +169,22 @@ namespace Magnefu
 		// -- Creating logical device(s) -- //
 
 		// Specifying queues to be created
-		QueueFamilyIndices indices = FindQueueFamilies(m_VkPhysicalDevice);
+		
+		std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+		queueCreateInfos.reserve(2);
+		std::set<uint32_t> uniqueQueueFamilies = { m_QueueFamilyIndices.GraphicsFamily.value(), m_QueueFamilyIndices.PresentFamily.value() };
 
-		VkDeviceQueueCreateInfo queueCreateInfo{};
-		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-		queueCreateInfo.queueFamilyIndex = indices.GraphicsFamily.value();
-		queueCreateInfo.queueCount = 1;
-
-		float queuePriority = 1.0f;
-		queueCreateInfo.pQueuePriorities = &queuePriority;
-
+		float queuePriority = 1.f;
+		for (uint32_t queueFamily : uniqueQueueFamilies)
+		{
+			VkDeviceQueueCreateInfo queueCreateInfo{};
+			queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueCreateInfo.queueFamilyIndex = queueFamily;
+			queueCreateInfo.queueCount = 1;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+			queueCreateInfos.push_back(queueCreateInfo);
+		}
+		
 		// Specifying device features to be used
 		VkPhysicalDeviceFeatures deviceFeatures{}; //empty for now
 
@@ -181,24 +192,77 @@ namespace Magnefu
 		// Create the logical device
 		VkDeviceCreateInfo logicalDevCreateInfo{};
 		logicalDevCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-
-
-		logicalDevCreateInfo.pQueueCreateInfos = &queueCreateInfo;
-		logicalDevCreateInfo.queueCreateInfoCount = 1;
-
+		logicalDevCreateInfo.pQueueCreateInfos = queueCreateInfos.data();
+		logicalDevCreateInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
 		logicalDevCreateInfo.pEnabledFeatures = &deviceFeatures;
+		logicalDevCreateInfo.enabledExtensionCount = deviceExtensions.size();
+		logicalDevCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
 		// Instantiate the logical device
 		if (vkCreateDevice(m_VkPhysicalDevice, &logicalDevCreateInfo, nullptr, &m_VkDevice) != VK_SUCCESS)
 			MF_CORE_ASSERT(false, "failed to create logical device!");
 		
 		// Retrieving queue handles
-		vkGetDeviceQueue(m_VkDevice, indices.GraphicsFamily.value(), 0, &m_GraphicsQueue); // that 0 is the queue family index
+		vkGetDeviceQueue(m_VkDevice, m_QueueFamilyIndices.GraphicsFamily.value(), 0, &m_GraphicsQueue); // that 0 is the queue family index
+		vkGetDeviceQueue(m_VkDevice, m_QueueFamilyIndices.PresentFamily.value(), 0, &m_PresentQueue); 
+																						
 
 		// ---------------------------------------- //
 
-		
 
+		// -- Creating the swap chain (infrastructure for the frame buffer) -- // TODO: Move to VKFrameBuffer class
+
+		SwapChainSupportDetails swapChainSupport = QuerySwapChainSupport(m_VkPhysicalDevice);
+
+		VkSurfaceFormatKHR surfaceFormat = ChooseSwapSurfaceFormat(swapChainSupport.Formats);
+		VkPresentModeKHR presentMode = ChooseSwapPresentMode(swapChainSupport.PresentModes);
+		VkExtent2D extent = ChooseSwapExtent(swapChainSupport.Capabilities);
+
+		uint32_t imageCount = swapChainSupport.Capabilities.minImageCount + 1;
+		if (swapChainSupport.Capabilities.maxImageCount > 0 && imageCount > swapChainSupport.Capabilities.maxImageCount) {
+			imageCount = swapChainSupport.Capabilities.maxImageCount;
+		}
+
+		VkSwapchainCreateInfoKHR swapChainCreateInfo{};
+		swapChainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		swapChainCreateInfo.surface = m_WindowSurface;
+		swapChainCreateInfo.minImageCount = imageCount;
+		swapChainCreateInfo.imageFormat = surfaceFormat.format;
+		swapChainCreateInfo.imageColorSpace = surfaceFormat.colorSpace;
+		swapChainCreateInfo.imageExtent = extent;
+		swapChainCreateInfo.imageArrayLayers = 1;
+
+		// NOTE Renders directly to swap chain image; Use VK_IMAGE_USAGE_TRANSFER_DST_BIT for post processing
+		swapChainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT; 
+
+		uint32_t queueFamilyIndices[] = { m_QueueFamilyIndices.GraphicsFamily.value(), m_QueueFamilyIndices.PresentFamily.value() };
+
+		if (m_QueueFamilyIndices.GraphicsFamily != m_QueueFamilyIndices.PresentFamily) {
+			swapChainCreateInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+			swapChainCreateInfo.queueFamilyIndexCount = 2;
+			swapChainCreateInfo.pQueueFamilyIndices = queueFamilyIndices;
+		}
+		else {
+			swapChainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+			swapChainCreateInfo.queueFamilyIndexCount = 0; // Optional
+			swapChainCreateInfo.pQueueFamilyIndices = nullptr; // Optional
+		}
+
+		swapChainCreateInfo.preTransform = swapChainSupport.Capabilities.currentTransform;
+		swapChainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+		swapChainCreateInfo.presentMode = presentMode;
+		swapChainCreateInfo.clipped = VK_TRUE;
+		swapChainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
+
+		if (vkCreateSwapchainKHR(m_VkDevice, &swapChainCreateInfo, nullptr, &m_SwapChain) != VK_SUCCESS)
+			MF_CORE_ASSERT(false, "Failed to create swap chain!");
+
+		// ---------------------------------------- //
+
+		/*MF_CORE_DEBUG("Renderer Info: ");
+		MF_CORE_DEBUG("\tVersion: {}", m_RendererInfo.Version);
+		MF_CORE_DEBUG("\tVendor: {}", m_RendererInfo.Vendor);
+		MF_CORE_DEBUG("\tRenderer: {}", m_RendererInfo.Renderer);*/
 
 	}
 
@@ -222,66 +286,37 @@ namespace Magnefu
 		vkGetPhysicalDeviceProperties(device, &deviceProperties);
 		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
-		QueueFamilyIndices indices = FindQueueFamilies(device);
+		m_QueueFamilyIndices = FindQueueFamilies(device);
+		bool extensionsSupported = CheckDeviceExtensionSupport(device);
 
-		if (
-			deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
-			indices.GraphicsFamily.has_value()
-			)
-		{
-			switch (deviceProperties.vendorID) {
-				case 0x10DE: 
-				{
-					m_RendererInfo.Vendor = "NVIDIA";
-					uint32_t driverVersion = deviceProperties.driverVersion;
-
-
-					/*uint32_t major = VK_VERSION_MAJOR(driverVersion);
-					uint32_t minor = VK_VERSION_MINOR(driverVersion);
-					uint32_t patch = VK_VERSION_PATCH(driverVersion);*/
-
-					//std::string major = std::to_string((driverVersion >> 22) & 0x3ff);
-					//std::string minor = std::to_string((driverVersion >> 17) & 0x1f);
-					//std::string patch = std::to_string(driverVersion & 0x1ffff);
-					//
-					//std::string strDriverVersion = major + "." + minor + "." + patch;
-					//
-					//m_RendererInfo.Version = strDriverVersion;
-					break;
-				}
-				case 0x1002: 
-				{
-					m_RendererInfo.Version = "AMD";
-					break;
-				}
-				case 0x8086: 
-				{
-					m_RendererInfo.Version = "Intel";
-					break;
-				}
-				case 0x13B5: 
-				{
-					m_RendererInfo.Version = "ARM";
-					break;
-				}
-				case 0x1010: 
-				{
-					m_RendererInfo.Version = "Imagination Technologies";
-					break;
-				}
-				case 0x5143: 
-				{
-					m_RendererInfo.Version = "Qualcomm";
-					break;
-				}
-			}
+		bool swapChainAdequate = false;
+		if (extensionsSupported) {
+			SwapChainSupportDetails swapChainSupport = QuerySwapChainSupport(device);
+			swapChainAdequate = !swapChainSupport.Formats.empty() && !swapChainSupport.PresentModes.empty();
+		}
 			
-			m_RendererInfo.Renderer = deviceProperties.deviceName;
+		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && 
+			m_QueueFamilyIndices.IsComplete() && 
+			extensionsSupported &&
+			swapChainAdequate;
+		
+	}
 
-			return true;
+	bool VKContext::CheckDeviceExtensionSupport(VkPhysicalDevice device)
+	{
+		uint32_t extensionCount;
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+		std::set<std::string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
+
+		for (const auto& extension : availableExtensions) {
+			requiredExtensions.erase(extension.extensionName);
 		}
 
-		return false;
+		return requiredExtensions.empty();
 	}
 
 	QueueFamilyIndices VKContext::FindQueueFamilies(VkPhysicalDevice device)
@@ -297,15 +332,160 @@ namespace Magnefu
 		int i = 0;
 		for (const auto& queueFamily : queueFamilies) 
 		{
-			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) 
+			VkBool32 presentSupport = false;
+			vkGetPhysicalDeviceSurfaceSupportKHR(device, i, m_WindowSurface, &presentSupport);
+
+			// Preferring a physical device that supports drawing and presentation in the same queue
+			if (presentSupport && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			{
+				indices.PresentFamily = i;
 				indices.GraphicsFamily = i;
+				MF_CORE_DEBUG("Supported Number of Queues: {}", queueFamily.queueCount);
+			}
+
+			/*if (presentSupport)
+				indices.PresentFamily = i;
+
+			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) 
+				indices.GraphicsFamily = i;*/
 
 			if (indices.IsComplete())
 				break;
-
+			
 			i++;
 		}
 
 		return indices;
 	}
+
+	SwapChainSupportDetails VKContext::QuerySwapChainSupport(VkPhysicalDevice device)
+	{
+		SwapChainSupportDetails details;
+
+		// Formats
+		uint32_t formatCount;
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, m_WindowSurface, &formatCount, nullptr);
+
+		if (formatCount != 0) 
+		{
+			details.Formats.resize(formatCount);
+			vkGetPhysicalDeviceSurfaceFormatsKHR(device, m_WindowSurface, &formatCount, details.Formats.data());
+		}
+
+		// Presentation Modes
+		uint32_t presentModeCount;
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, m_WindowSurface, &presentModeCount, nullptr);
+
+		if (presentModeCount != 0) {
+			details.PresentModes.resize(presentModeCount);
+			vkGetPhysicalDeviceSurfacePresentModesKHR(device, m_WindowSurface, &presentModeCount, details.PresentModes.data());
+		}
+
+		// Capabilities
+		vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, m_WindowSurface, &details.Capabilities);
+
+		return details;
+	}
+
+	VkSurfaceFormatKHR VKContext::ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats)
+	{
+		for (const auto& availableFormat : availableFormats) {
+			if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0];
+	}
+
+	VkPresentModeKHR VKContext::ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes)
+	{
+		for (const auto& availablePresentMode : availablePresentModes) {
+			if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+				return availablePresentMode;
+			}
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	VkExtent2D VKContext::ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities)
+	{
+		if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) 
+		{
+			return capabilities.currentExtent;
+		}
+		else
+		{
+			int width, height;
+			glfwGetFramebufferSize(m_WindowHandle, &width, &height);
+
+			VkExtent2D actualExtent = {
+				static_cast<uint32_t>(width),
+				static_cast<uint32_t>(height)
+			};
+
+			actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+			actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+			return actualExtent;
+		}
+	}
 }
+
+
+
+
+
+//switch (deviceProperties.vendorID) {
+//case 0x10DE:
+//{
+//	m_RendererInfo.Vendor = "NVIDIA";
+//	break;
+//}
+//case 0x1002:
+//{
+//	m_RendererInfo.Vendor = "AMD";
+//	m_RendererInfo.Version = "INSERT GPU VERSION HERE";
+//	break;
+//}
+//case 0x8086:
+//{
+//	m_RendererInfo.Vendor = "Intel";
+//	m_RendererInfo.Version = "INSERT GPU VERSION HERE";
+//	break;
+//}
+//case 0x13B5:
+//{
+//	m_RendererInfo.Vendor = "ARM";
+//	m_RendererInfo.Version = "INSERT GPU VERSION HERE";
+//	break;
+//}
+//case 0x1010:
+//{
+//	m_RendererInfo.Vendor = "Imagination Technologies";
+//	m_RendererInfo.Version = "INSERT GPU VERSION HERE";
+//	break;
+//}
+//case 0x5143:
+//{
+//	m_RendererInfo.Vendor = "Qualcomm";
+//	m_RendererInfo.Version = "INSERT GPU VERSION HERE";
+//	break;
+//}
+//default:
+//{
+//	m_RendererInfo.Vendor = "UNKNOWN";
+//	m_RendererInfo.Version = "UNKNOWN";
+//}
+//}
+//
+//
+//
+//uint32_t driverVersion = deviceProperties.driverVersion;
+//std::string major = std::to_string(VK_VERSION_MAJOR(driverVersion));
+//std::string minor = std::to_string(VK_VERSION_MINOR(driverVersion));
+//std::string patch = std::to_string(VK_VERSION_PATCH(driverVersion));
+//
+//m_RendererInfo.Version = major + "." + minor + "." + patch;
+//m_RendererInfo.Renderer = deviceProperties.deviceName;
--- Magnefu/src/Platform/VK/VKContext.h
+++ Magnefu/src/Platform/VK/VKContext.h
@@ -7,12 +7,23 @@ struct GLFWwindow;
 
 namespace Magnefu
 {
+	static const std::vector<const char*> deviceExtensions = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	};
+
 	struct QueueFamilyIndices {
 		std::optional<uint32_t> GraphicsFamily;
+		std::optional<uint32_t> PresentFamily;
 
 		bool IsComplete() {
-			return GraphicsFamily.has_value();
+			return (GraphicsFamily.has_value() && PresentFamily.has_value());
 		}
+	};
+
+	struct SwapChainSupportDetails {
+		std::vector<VkSurfaceFormatKHR> Formats;
+		std::vector<VkPresentModeKHR>   PresentModes;
+		VkSurfaceCapabilitiesKHR        Capabilities;
 	};
 
 	class VKContext : public GraphicsContext
@@ -27,15 +38,24 @@ namespace Magnefu
 
 	private:
 		bool IsDeviceSuitable(VkPhysicalDevice);
+		bool CheckDeviceExtensionSupport(VkPhysicalDevice);
 		QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice);
+		SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice);
+		VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+		VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+		VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
 
 	private:
-		GLFWwindow*      m_WindowHandle;
-		RendererInfo     m_RendererInfo;
-		VkInstance       m_VkInstance;
-		VkPhysicalDevice m_VkPhysicalDevice;
-		VkDevice         m_VkDevice;
-		VkQueue          m_GraphicsQueue;
-		VkSurfaceKHR     m_WindowSurface;
+		GLFWwindow*             m_WindowHandle;
+		RendererInfo            m_RendererInfo;
+		VkInstance              m_VkInstance;
+		VkPhysicalDevice        m_VkPhysicalDevice;
+		VkDevice                m_VkDevice;
+		VkQueue                 m_GraphicsQueue;
+		VkSurfaceKHR            m_WindowSurface;
+		VkQueue			        m_PresentQueue;
+		QueueFamilyIndices      m_QueueFamilyIndices;
+		SwapChainSupportDetails m_SwapChainSupportDetails;
+		VkSwapchainKHR          m_SwapChain;
 	};
 }
