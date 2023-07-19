ab9a0dd368efdc5af6640afeb1b70ba095abc58a
Leon Thompson <ltyronethompson09@gmail.com>
2023-07-19 04:31:12 -0600

    more shader setup

--- Magnefu/src/Magnefu/Renderer/Shader.h
+++ Magnefu/src/Magnefu/Renderer/Shader.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include "Magnefu/ResourceManagement/Pool.h"
+
 
 namespace Magnefu
 {
+	enum ShaderStage
+	{
+		SHADER_STAGE_VERTEX,
+		SHADER_STAGE_FRAGMENT,
+		SHADER_STAGE_GEOMETRY,
+		SHADER_STAGE_COMPUTE,
+		SHADER_STAGE_TESSELATION,
+		SHADER_STAGE_VERTEX_AND_FRAGMENT
+	};
+
+	enum DynamicState
+	{
+		DYNAMIC_STATE_VIEWPORT = 0,
+		DYNAMIC_STATE_SCISSOR,
+	};
+
 	struct ViewportInfo
 	{
 		uint32_t ViewportCount;
@@ -31,10 +49,52 @@ namespace Magnefu
 		CullMode CullMode;
 	};
 
-	enum DynamicState
+	enum MSAASampleCountFlag
 	{
-		DYNAMIC_STATE_VIEWPORT = 0,
-		DYNAMIC_STATE_SCISSOR,
+		SAMPLE_COUNT_1_BIT = 0x00000001,
+		SAMPLE_COUNT_2_BIT = 0x00000002,
+		SAMPLE_COUNT_4_BIT = 0x00000004,
+		SAMPLE_COUNT_8_BIT = 0x00000008,
+		SAMPLE_COUNT_16_BIT = 0x00000010,
+		SAMPLE_COUNT_32_BIT = 0x00000020,
+		SAMPLE_COUNT_64_BIT = 0x00000040
+	};
+
+	struct MSAAInfo
+	{
+		MSAASampleCountFlag SampleCount;
+		bool EnableSampleShading;
+		float MinSampleShading;
+	};
+
+	enum DepthCompareOp
+	{
+		COMPARE_OP_NEVER = 0,
+		COMPARE_OP_LESS = 1,
+		COMPARE_OP_EQUAL = 2,
+		COMPARE_OP_LESS_OR_EQUAL = 3,
+		COMPARE_OP_GREATER = 4,
+		COMPARE_OP_NOT_EQUAL = 5,
+		COMPARE_OP_GREATER_OR_EQUAL = 6,
+		COMPARE_OP_ALWAYS = 7,
+	};
+	
+	struct DepthAndStencilInfo
+	{
+		bool DepthTestEnable;
+		bool DepthWriteEnable;
+		DepthCompareOp CompareOp;
+		bool DepthBoundsTestEnable;
+		bool StencilTestEnable;
+
+	};
+
+	struct PushConstantInfo
+	{
+		bool Enabled;
+		ShaderStage Stages;
+		uint32_t Offset;
+		uint32_t Size;
 	};
 
 	struct GraphicsPipelineState
@@ -42,19 +102,11 @@ namespace Magnefu
 		std::vector<DynamicState> DynamicStates;
 		ViewportInfo ViewportInfo;
 		RasterizerInfo RasterizerInfo;
+		MSAAInfo MSAAInfo;
+		DepthAndStencilInfo DepthAndStencilInfo;
+		PushConstantInfo PushConstantInfo;
 	};
 
-
-
-	enum ShaderStage
-	{
-		SHADER_STAGE_VERTEX,
-		SHADER_STAGE_FRAGMENT,
-		SHADER_STAGE_GEOMETRY,
-		SHADER_STAGE_COMPUTE,
-		SHADER_STAGE_TESSELATION,
-		SHADER_STAGE_VERTEX_AND_FRAGMENT
-	};
 
 	struct ShaderStageDesc
 	{
@@ -84,8 +136,9 @@ namespace Magnefu
 		const char*      DebugName;
 		const char*      Path;
 		ShaderStageDescs StageDescriptions;
+		Handle<BindGroup> BindGroups[2];
+		GraphicsPipelineState GraphicsPipeline;
 	};
-
 
 
 	class Shader
